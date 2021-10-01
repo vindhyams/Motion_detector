1a3a0b3e6ef57c5d89202000caed7e3799318b4d
vindhya <vindhyams27@gmail.com>
2021-10-01 19:18:27 +0530

    motion detector

---
+++ webcam.py
@@ -0,0 +1,50 @@
+import cv2,time
+
+first_frame=None
+
+
+video=cv2.VideoCapture(0)
+
+while True:
+  
+    check, frame= video.read()
+    
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+
+    if first_frame is None:
+        first_frame=gray
+        continue
+   
+    delta_frame=cv2.absdiff(first_frame,gray)
+
+    thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
+
+    thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
+
+    (cnts,_) =cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour)<1000:
+            continue
+        (x,y,w,h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,225,0),3)
+
+    cv2.imshow("captured gray",gray)
+    cv2.imshow("deltaframe",delta_frame)
+    cv2.imshow("thresh frmae",thresh_frame)
+    cv2.imshow("colorframe",frame)
+    key=cv2.waitKey(1)
+    print(gray)
+    print(delta_frame)
+    if key==ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+
+#in this code we can detect the moving objects and put in the rectangular box
+#basic idea first we will fix the background image and compare the next image with the backroung image 
+#if chnages founf we will put it in the rectangular box 
+#for the we use gary image and shadows and thresh frame
